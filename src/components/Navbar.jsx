5b1425233378cce8f609d83188596c5a470303d8
sundar_2802 <124782589+Sundar2802@users.noreply.github.com>
2023-11-21 21:43:19 +0530

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,33 @@
-// Navbar.jsx
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button, Container, Avatar } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, Container, Avatar, Grid} from '@mui/material';
 import { Link } from 'react-router-dom';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+
+
+import eventImage from 'E:\\event-planning3-edited.jpg'; 
+import EventBox from './Eventbox';
+import Footer from './Nfooter';
+
 
 const Navbar = () => {
   return (
-    <div style={{ minHeight: '100vh' }}>
+    <div style={{ minHeight: '10vh' }}>
       <AppBar position="static" style={{ backgroundColor: '#333' }}>
         <Toolbar>
+          <h2>EVENT MANAGEMENT</h2>
           <Typography variant="h6" style={{ flexGrow: 1, textDecoration: 'none', color: 'white' }}>
-            Event Management
           </Typography>
-          
           <Button color="inherit" href="#home">Home</Button>
           <Button color="inherit" href="#about">About</Button>
           <Button color="inherit" href="#contact">Contact</Button>
-         <Link to = '/Login'><Button variant="outlined">Login</Button></Link>
-          <Avatar sx={{ bgcolor: 'deepOrange[500]' }}>N</Avatar>
+          <Link to='/Login'><Button variant="outlined" sx={{marginRight:"10px"}}>Login</Button></Link>
+          <Avatar sx={{ bgcolor: 'deepOrange[500]' }}><AccountCircleOutlinedIcon/></Avatar>
         </Toolbar>
       </AppBar>
-      <Container maxWidth="md" style={{ marginTop: '20px', padding: '20px', backgroundColor: '#f4f4f4', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', height: '120vh' }}>
+      <Container maxWidth="md" style={{ marginTop: '20px', padding: '20px', backgroundColor: '#f4f4f4', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', height: '140vh' }}>
+        
+        <img src={eventImage} alt="Event Management" style={{ width: '100%', marginBottom: '20px', borderRadius: '8px' }} />
+
         <Typography variant="h4" gutterBottom>
           Welcome to Our Event Management Platform
         </Typography>
@@ -29,10 +37,17 @@ const Navbar = () => {
         <Typography variant="body1" paragraph>
           Explore our services and make your event unforgettable!
         </Typography>
+        <Grid container spacing={1}>
+          <EventBox title="Social Event" description="Birthday parties,Anniversary celebrations,Graduation parties" />
+          <EventBox title="Wedding" description="Ceremony,Reception,Engagement party,Bridal shower etc.." />
+          <EventBox title="Corporate Events" description="Conferences,Seminars,Workshops,Team-building events,Product launches" />
+          <EventBox title="Concerts and Performances" description="Music concerts,Dance performances" />
+          <EventBox title="Sporting Events" description="Tournaments,Matches,Sports day events" />
+          <EventBox title="Holiday Events" description="Christmas parties,New Year's Eve celebrations" />
+        </Grid>
       </Container>
-      <footer style={{ marginTop: '20px', padding: '10px', backgroundColor: '#333', color: 'white', textAlign: 'center' }}>
-        <p>&copy; 2023 Your Event Management. All rights reserved.</p>
-      </footer>
+      <Footer/>
+      
     </div>
   );
 };
